e302ca4d6f9fe7cc52f8d69f86f995a4529efd77
vladikko <iphone24.vk@gmail.com>
2022-08-09 12:03:15 +0200

    ERTB 2.0 Beta 4.2
    Updated GetCryptoRates

--- GetExchangeRates.py
+++ GetExchangeRates.py
@@ -3,6 +3,7 @@ import requests
 import time
 from Token import apiKey
 from DBH import UpdateExchangeRatesDB, GetExchangeRates, UpdateCryptoRatesDB, GetCryptoRates
+from random import randint
 
 #Own libraries
 from NewPrint import Print
@@ -21,7 +22,7 @@ def SheduleCryptoUpdate():
     global cryptoRates
     while True:
         cryptoRates = UpdateCryptoRates()
-        time.sleep(10)
+        time.sleep(randint(10,30))  
 
 def UpdateExchangeRates() -> dict:
     global exchangeRates
@@ -52,7 +53,40 @@ def UpdateCryptoRates() -> dict:
         UpdateCryptoRatesDB(cryptoRates.copy())
         Print("Updating of crypto rates is successfull.", "S")
     except:
-        Print("Updating CR failed.", "E")
-        Print("Using crypto rates from DB.", "S")
-        cryptoRates = GetCryptoRates()
+        Print("Updating CR failed. Trying different endpoint", "E")
+        try:
+            url = "https://api1.binance.com/api/v3/ticker/price"
+            response = requests.get(url)
+            cryptoRates = {}
+            for pair in response.json():
+                if pair['symbol'].find("USDT") != -1 and any(pair['symbol'][:-4] == s for s in cryptoList):
+                    cryptoRates[pair['symbol'][:-4]]=float(pair['price'])
+            UpdateCryptoRatesDB(cryptoRates.copy())
+            Print("Updating of crypto rates is successfull.", "S")
+        except:
+            Print("Updating CR failed. Trying different endpoint", "E")
+            try:
+                url = "https://api2.binance.com/api/v3/ticker/price"
+                response = requests.get(url)
+                cryptoRates = {}
+                for pair in response.json():
+                    if pair['symbol'].find("USDT") != -1 and any(pair['symbol'][:-4] == s for s in cryptoList):
+                        cryptoRates[pair['symbol'][:-4]]=float(pair['price'])
+                UpdateCryptoRatesDB(cryptoRates.copy())
+                Print("Updating of crypto rates is successfull.", "S")
+            except:
+                Print("Updating CR failed. Trying different endpoint", "E")
+                try:
+                    url = "https://api3.binance.com/api/v3/ticker/price"
+                    response = requests.get(url)
+                    cryptoRates = {}
+                    for pair in response.json():
+                        if pair['symbol'].find("USDT") != -1 and any(pair['symbol'][:-4] == s for s in cryptoList):
+                            cryptoRates[pair['symbol'][:-4]]=float(pair['price'])
+                    UpdateCryptoRatesDB(cryptoRates.copy())
+                    Print("Updating of crypto rates is successfull.", "S")
+                except:
+                    Print("Using crypto rates from DB.", "S")
+                    cryptoRates = GetCryptoRates()
+        
     return cryptoRates.copy()
